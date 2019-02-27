a3f9724ebdd27e4f1f04d228991bcb1d4d5e897a
root <root@ip-172-31-20-13.ec2.internal>
2019-02-27 09:45:19 +0000

    ccommit_pipeline_fargate

---
+++ nodeapps_osmo/server.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const express = require('express');
+const logger = require('./logger').logger;
+
+// Constants
+const PORT = 3010;
+const HOST = '0.0.0.0';
+
+// App
+const app = express();
+app.get('/', (req, res) => {
+    logger.debug({ req: req.originalUrl, method: req.method }, 'Received a Request');
+    logger.info({ req: req.originalUrl, method: req.method, resStatus: res.statusCode }, 'Sending Response');
+    logger.error('There was no Error');
+    res.send('Welcome to Osmoses 27 Feb!!..\n');
+});
+
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
